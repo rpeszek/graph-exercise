647ea7a11af9f213cc150fda73e8678307d08b0e
rpeszek <peszeks@gmail.com>
2018-01-10 16:31:34 -0700

    Changed to use safe maximumBy

--- src/Solution.hs
+++ src/Solution.hs
@@ -5,7 +5,8 @@ import Dag (Dag, topologicalorder)
 import Data.Graph.Inductive.Graph (Node, suc)
 import qualified Data.HashMap.Lazy as HM
 import Data.Ord (comparing)
-import Data.List (maximumBy, length, foldl')
+import Data.List (length, foldl')
+import Safe.Foldable (maximumByDef)
 
 type Path = [Node]
 
@@ -19,10 +20,7 @@ longestPath node graph =
          children = suc graph
          logic ::  HM.HashMap Node Path -> Node -> HM.HashMap Node Path
          logic acc cnode =  
-             let childrenPaths = map (flip (HM.lookupDefault []) acc) $ children cnode
-                 path = if null childrenPaths 
-                        then [cnode] 
-                        else ((:) cnode) . maximumBy (comparing length) $ childrenPaths
+             let path = ((:) cnode) . maximumByDef [] (comparing length) . map (flip (HM.lookupDefault []) acc) $ children cnode
              in HM.insert cnode path acc
          pathPerNode = foldl' logic HM.empty (reverse . topologicalorder $ graph)
     in HM.lookup node pathPerNode 
