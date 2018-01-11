a5f8c7323314ad7cc581c668ba31a09d1ecd3575
rpeszek <peszeks@gmail.com>
2018-01-10 21:58:34 -0700

    Documentation

--- src/IncorrectSolution.hs
+++ src/IncorrectSolution.hs
@@ -4,6 +4,10 @@ https://stackoverflow.com/questions/40647260/finding-the-longest-path-through-a-
 This solution correctly computes the longest path in a spanning tree but that is not the same as the longest path 
 in the graph.  Graph can have many spanning trees of possibly different heights. 
 
+Considering that longest path does not have good solution for general graphs
+https://en.wikipedia.org/wiki/Longest_path_problem
+just looking at type signature of the stackoverflow solution makes its validity a suspect. 
+
 See test/IncorrectSolutionSpec.hs for counterexamples.
 -}
 
@@ -17,4 +21,3 @@ import Data.List (maximumBy, length)
 
 bftLongestPath :: Graph gr => Node -> gr a b  -> [Node]
 bftLongestPath node graph = maximumBy (comparing length) (bft node graph)
-
